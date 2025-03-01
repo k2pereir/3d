83e154cda172ad4b07196604e56fcbf99dd9b9be
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2025-03-01 11:14:01 -0500

    there are erorrss but i don't want to lose this

--- live_depth.py
+++ live_depth.py
@@ -1,6 +1,5 @@
 import torch
 import cv2 as cv 
-import numpy as np
 import matplotlib.pyplot as plt
 
 #load model 
--- transform.py
+++ transform.py
@@ -1,18 +1,21 @@
-import torch
 import cv2 as cv 
-import matplotlib.pyplot as plt
+import torch 
+import matplotlib.pyplot as plt 
+import open3d 
+import numpy as np 
 
 #load model 
 midas = torch.hub.load("intel-isl/MiDaS", "MiDaS_small")
 midas.to(torch.device("cpu"))
 midas.eval()
 transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
-transform = transforms.small_transform 
+transform = transforms.small_transform
 
+#get 2d image 
 img = cv.imread("penguin.jpg")
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
-#monocular depth estimation
+#get depth map 
 input_batch = transform(img).to('cpu')
 with torch.no_grad(): 
     prediction = midas(input_batch)
@@ -23,5 +26,15 @@ with torch.no_grad():
         align_corners=False,
     ).squeeze()
 output = prediction.cpu().numpy()
+
+print("!!!!!!!!!")
+print(output)
+print("!!!!!!!!!")
+
 plt.imshow(output)
-plt.show()
+plt.savefig('depthmap.png')
+
+#get 3d point cloud
+pcd = open3d.t.geometry.PointCloud(
+    np.array(output, dtype=np.float32))
+print(pcd)
